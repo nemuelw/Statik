0bfcbe5a9b0db4c4cb897c51ca8c98dbe0a91c0d
Nemuel Wainaina <bR0S3@protonmail.com>
2023-03-07 09:37:17 +0300

    Add .gitignore

--- statik.py
+++ statik.py
@@ -1,10 +1,18 @@
 # Author : Nemuel Wainaina
 # Script to automate Static Malware Analysis
 
-
+import requests
 
 class MalwareSample:
-    pass
+    
+    def __init__(self, file) -> None:
+        self.sample = file
+
+    def generate_hashes(self):
+        pass
+
+    def generate_fuzzy_hash(self):
+        pass
 
 if __name__ == "__main__":
     pass
