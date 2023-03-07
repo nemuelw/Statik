9d8234b7653d72577143fc12df6b3ad9d5e7d0c1
Nemuel Wainaina <bR0S3@protonmail.com>
2023-03-07 10:14:47 +0300

    Add the request package

--- statik.py
+++ statik.py
@@ -1,6 +1,12 @@
 # Author : Nemuel Wainaina
 # Script to automate Static Malware Analysis
 
+import hashlib
+import os
+import sys
+
+from colorama import init, Fore
+
 class MalwareSample:
     
     def __init__(self, file) -> None:
@@ -13,4 +19,13 @@ class MalwareSample:
         pass
 
 if __name__ == "__main__":
-    pass
+    init()
+    GREEN = Fore.GREEN
+    RED = Fore.RED
+    GRAY = Fore.LIGHTBLACK_EX
+    RESET = Fore.RESET
+
+    args = sys.argv
+    if len(args) != 2:
+        print(f"{RED} [!] {RESET} Syntax : python3 statik.py <sample_file>")
+        exit(1)
